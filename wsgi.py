5411ce1c95de2e551e9c05d60015b6bbb232e760
Nelson Carrasquel <carrasquel@outlook.com>
2020-11-20 14:25:32 -0400

    wsgi file fixed

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,11 @@
 def get_app():
     
     from app import create_app
+    
+    app = create_app()
 
-    return create_app()
+    app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///database.db'
+
+    return app
 
 app = get_app()
